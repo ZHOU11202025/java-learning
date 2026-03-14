be23271ef5eb9805e1a9f4b27fba9e7b8f36b548
Zhou Chengxi <267263097+ZHOU11202025@users.noreply.github.com>
2026-03-14 18:34:58 +0800

    添加 FactorialDoWhile 程序

---
+++ src/FactorialDoWhile.java
@@ -0,0 +1,20 @@
+public class FactorialDoWhile {
+    public static void main(String[] args){
+        //使用do...while循环语句计算5的阶乘
+        int i = 5;
+        int factorial = 1;
+        do{
+            factorial *= i;
+            i--;
+        }while(i > 0);
+        System.out.println("5! = " + factorial);
+        //1递增到5
+        int n = 1;
+        int factorial1 = 1;
+        do{
+            factorial1 *= n;
+            n++;
+        }while(n <= 5);
+        System.out.println("5! = " + factorial1);
+    }
+}
