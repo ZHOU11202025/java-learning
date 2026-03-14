85f71b03e46a918b7e995dee1a4154c78153b172
Zhou Chengxi <267263097+ZHOU11202025@users.noreply.github.com>
2026-03-14 18:19:41 +0800

    添加 Sum1To99Odd 程序

---
+++ src/Sum1To99Odd.java
@@ -0,0 +1,14 @@
+public class Sum1To99Odd {
+    public static void main(String[] args){
+        //计算1+3+5+9+...+99的值
+        int i = 1;
+        int sum = 0;
+        while(i < 100){
+            if(i % 2 != 0){
+                sum += i;
+            }
+            i++;
+        }
+        System.out.println("1+3+5+9+...+99 = " + sum);
+    }
+}
