908c87047daf2b68899fe01c874fe5443bfaf9d9
Zhou Chengxi <267263097+ZHOU11202025@users.noreply.github.com>
2026-03-16 22:20:52 +0800

    添加 Sum1To100For 程序

---
+++ src/Sum1To100For.java
@@ -0,0 +1,10 @@
+public class Sum1To100For {
+    public static void main(String[] args) {
+        //计算1到100的和（用for循环）
+        int sum = 0;
+        for(int i = 1;i <= 100;i++){
+            sum += i;
+        }
+        System.out.println("1+2+3+...+100 = " + sum);
+    }
+}
