9d5e1f9007b220bc5727411043982cdb31fb8b6a
Zhou Chengxi <267263097+ZHOU11202025@users.noreply.github.com>
2026-03-18 22:42:50 +0800

    添加 DivisibleBy3Sum 程序

---
+++ src/DivisibleBy3Sum.java
@@ -0,0 +1,14 @@
+public class DivisibleBy3Sum {
+    public static void main(String[] args){
+        //计算1到100中所有能被3整除的数的和（用while循环）
+        int i = 1;
+        int sum = 0;
+        while(i <= 100){
+            if(i % 3 == 0){
+                sum += i;
+            }
+            i++;
+        }
+        System.out.println("1到100中所有能被3整除的数的和为 " + sum);
+    }
+}
