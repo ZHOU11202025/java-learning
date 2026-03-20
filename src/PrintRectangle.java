037c8a4dffd2093a060c9cd912113106931f11f9
Zhou Chengxi <267263097+ZHOU11202025@users.noreply.github.com>
2026-03-20 16:15:07 +0800

    优化 PrintRectangle 程序：消除冗余警告

--- src/PrintRectangle.java
+++ src/PrintRectangle.java
@@ -1,8 +1,7 @@
 public class PrintRectangle {
     public static void main(String[] args) {
         //使用两层for循环，打印五行五列的星号矩形
-        int i = 0;
-        int j = 0;
+        int i , j;
         for(i = 0;i < 5;i++){
             for(j = 0;j < 5;j++){
                 System.out.print("*");
