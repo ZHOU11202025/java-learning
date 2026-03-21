9c3ac2ed2366304cc072dcb69e719d66f273dd6e
Zhou Chengxi <267263097+ZHOU11202025@users.noreply.github.com>
2026-03-21 17:47:46 +0800

    完成数组基本操作练习

---
+++ src/ArrayPrint.java
@@ -0,0 +1,9 @@
+public class ArrayPrint {
+    public static void main(String[] args){
+        //创建数组{1，2，3，4，5}并打印
+        int[] a = {1,2,3,4,5};
+        for(int i = 0;i < a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
