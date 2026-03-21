697adc6834085cb3a5b0f945b2cf319aea3c65c7
Zhou Chengxi <267263097+ZHOU11202025@users.noreply.github.com>
2026-03-21 18:08:34 +0800

    完成 数组比较遍历 练习

---
+++ src/ArrayMax.java
@@ -0,0 +1,13 @@
+public class ArrayMax {
+    public static void main(String[] args){
+        //找数组{78，92，85，64，99}的最大值
+        int[] arr = {78, 92, 85, 64, 99};
+        int max = arr[0];
+        for(int i : arr){
+            if(i > max){
+                max = i;
+            }
+        }
+        System.out.println("该数组的最大值为 " + max);
+    }
+}
