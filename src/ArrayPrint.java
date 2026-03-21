4561c6f329c7556123f41c6c502511a66ef748a1
Zhou Chengxi <267263097+ZHOU11202025@users.noreply.github.com>
2026-03-21 17:59:43 +0800

    优化 ：增强for循环

--- src/ArrayPrint.java
+++ src/ArrayPrint.java
@@ -2,8 +2,8 @@ public class ArrayPrint {
     public static void main(String[] args){
         //创建数组{1，2，3，4，5}并打印
         int[] a = {1,2,3,4,5};
-        for(int i = 0;i < a.length;i++){
-            System.out.println(a[i]);
+        for(int i : a){
+            System.out.println(i);
         }
     }
 }
