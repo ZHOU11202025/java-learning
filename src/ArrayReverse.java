20686050eefedd8d787abe2e348fd9a32d4a8554
Zhou Chengxi <267263097+ZHOU11202025@users.noreply.github.com>
2026-03-24 14:40:14 +0800

    完成 数组反转 练习

---
+++ src/ArrayReverse.java
@@ -0,0 +1,13 @@
+public class ArrayReverse {
+    public static void main(String[] args){
+        //将数组{1，2，3，4，5}反转成{5，4，3，2，1}并打印
+        int[] arr1 = {1,2,3,4,5};
+        int[] arr2 = new int[arr1.length];
+        for(int i = 0;i < arr1.length;i++){
+            arr2[arr1.length - 1 - i] = arr1[i];
+        }
+        for(int j = 0;j < arr1.length;j++){
+            System.out.print(arr2[j]+" ");
+        }
+    }
+}
