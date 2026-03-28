e00ab2c02d45f6b1e438b4769622716b8e315005
Zhou Chengxi <267263097+ZHOU11202025@users.noreply.github.com>
2026-03-28 21:39:06 +0800

    Remove unnecessary initial value

--- src/nowcoder/SummationOfArray.java
+++ src/nowcoder/SummationOfArray.java
@@ -5,7 +5,7 @@ public class SummationOfArray {
         //有数列为：9，99，999，...，9999999999。要求使用程序计算此数列的和，并在控制台输出结果。（请尝试使用循环的方式生成这个数列并同时在循环中求和）,将该数列的和直接输出
         //write your code here........
         long sum = 0;
-        long num = 0;
+        long num;
         long i = 0;
         for(int n = 0;n < 10;n++){
             num = 9 + 10 * i;
