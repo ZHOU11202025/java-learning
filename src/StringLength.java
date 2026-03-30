06e76762dda09f31bcdc827ce3f764343c6e4bdf
Zhou Chengxi <267263097+ZHOU11202025@users.noreply.github.com>
2026-03-30 22:00:32 +0800

    Complete StringLength exercise

--- src/StringLength.java
+++ src/StringLength.java
@@ -1,8 +1,9 @@
-//import java.util.Scanner;
-//public class StringLength {
-//    public static void main(String[] args){
-//        //输入一个字符串，输出它的长度
-//        Scanner scanner = new Scanner(System.in);
-//        String a = scanner.nextString();
-//    }
-//}
+import java.util.Scanner;
+public class StringLength {
+    public static void main(String[] args){
+        //输入一个字符串，输出它的长度
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();//scanner.next()会以空格为分隔符，会读取到空格停止，nextLine能包含空格
+        System.out.println(a.length());
+    }
+}
