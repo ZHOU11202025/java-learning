05e6a6912f420482c0ce314a3855319c66674c3c
Zhou Chengxi <267263097+ZHOU11202025@users.noreply.github.com>
2026-04-29 23:05:00 +0800

    Fix formatting issues

--- src/practice/ScoreCalculator.java
+++ src/practice/ScoreCalculator.java
@@ -1,6 +1,6 @@
 package practice;
 //有三个方法，分别返回姓名和两门课成绩（总分），返回姓名和三门课成绩（总分），返回姓名和五门课成绩（总分）。
-// 封装成一个可变参数的方法
+//封装成一个可变参数的方法
 public class ScoreCalculator {
     public static void main(String[] args){
         Score s =  new Score();
