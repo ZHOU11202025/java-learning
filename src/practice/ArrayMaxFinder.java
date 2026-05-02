227675aa5413d93b1b902c8daaf9b1d6468ac52f
Zhou Chengxi <267263097+ZHOU11202025@users.noreply.github.com>
2026-05-02 11:17:36 +0800

    Improve code robustness

--- src/practice/ArrayMaxFinder.java
+++ src/practice/ArrayMaxFinder.java
@@ -3,18 +3,29 @@ package practice;
 public class ArrayMaxFinder {
     public static void main(String[] args){
         A01 a =  new A01();
-        double[] arr = {2,4,5.5,7};
-        System.out.println("arr数组的最大值是：" + a.max(arr));
+        double[] arr = {2,4,5.6,1};
+        Double res = a.max(arr);
+        if(res != null){
+            System.out.println("arr数组的最大值是：" + a.max(arr));
+        }
+        else{
+            System.out.println("arr数组输入有误");
+        }
     }
 }
 class A01{
-    public double max(double[] arr){
-        double max = arr[0];
-        for(int i = 1; i < arr.length; i++){
-            if(arr[i] > max){
-                max = arr[i];
+    public Double max(double[] arr){//包装类Double可以保存double小数，也可以保存null
+        if(arr != null && arr.length > 0){
+            double max = arr[0];
+            for(int i = 1; i < arr.length; i++){
+                if(arr[i] > max){
+                    max = arr[i];
+                }
             }
+            return max;
         }
-        return max;
+        else{
+            return null;
+        }
     }
 }
