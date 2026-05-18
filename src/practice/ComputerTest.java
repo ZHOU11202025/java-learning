f9b07d0788a8b3544605acc57ee723428a89cc7f
Zhou Chengxi <267263097+ZHOU11202025@users.noreply.github.com>
2026-05-18 14:53:29 +0800

    Use super constructor to initialize parent class fields

--- src/practice/ComputerTest.java
+++ src/practice/ComputerTest.java
@@ -7,26 +7,17 @@ package practice;
 //赋值，以及从Computer类继承的属性赋值，并使用方法并打印输出信息。
 public class ComputerTest {
     public static void main(String[] args) {
-        PC pc = new PC();
-        NotePad np = new NotePad();
-        pc.brand = "Honor";
-        pc.CPU = "Intel";
-        pc.memory = "16GB";
-        pc.hardDisk = "512GB";
-        System.out.println("品牌：" + pc.brand + " " + pc.getDetails());
+        PC pc = new PC("Intel","16GB","512GB","Honor");
+        System.out.println("品牌：" + pc.getBrand() + " " + pc.getDetails());
 
-        np.color = "black";
-        np.CPU = "AMD";
-        np.memory = "8GB";
-        np.hardDisk = "256GB";
-        System.out.println("颜色：" + np.color + " " + np.getDetails());
+        NotePad np = new NotePad("AMD","8GB","256GB","black");
+        System.out.println("颜色：" + np.getColor() + " " + np.getDetails());
     }
 }
 class Computer{
     String CPU;
     String memory;
     String hardDisk;
-    public Computer(){}
     public Computer(String CPU, String memory, String hardDisk){
         this.CPU = CPU;
         this.memory = memory;
@@ -37,8 +28,22 @@ class Computer{
     }
 }
 class PC extends Computer{
-    String brand;
+    private String brand;
+    public PC(String CPU, String memory, String hardDisk,String brand){
+        super(CPU, memory, hardDisk);
+        this.brand = brand;
+    }
+    public String getBrand(){
+        return brand;
+    }
 }
 class NotePad extends Computer{
-    String color;
+    private String color;
+    public NotePad(String CPU, String memory, String hardDisk,String color){
+        super(CPU, memory, hardDisk);
+        this.color = color;
+    }
+    public String getColor(){
+        return color;
+    }
 }
