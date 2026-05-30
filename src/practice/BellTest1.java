6e05636e002f12dc9c85f74f6ee48f09497a14f6
Zhou Chengxi <267263097+ZHOU11202025@users.noreply.github.com>
2026-05-30 12:32:29 +0800

    Complete BellTest1 exercise

---
+++ src/practice/BellTest1.java
@@ -0,0 +1,29 @@
+package practice;
+//1.有一个铃声接口Bell，里面有个ring方法。(右图)
+//2.有一个手机类Cellphone，具有闹钟功能alarmClock，参数是Bell类型(右图)
+//3.测试手机类的闹钟功能，通过匿名内部类(对象)作为参数，打印:懒猪起床了
+//4.再传入另一个匿名内部类(对象)，打印:小伙伴上课了
+public class BellTest1 {
+    public static void main(String[] args) {
+        new CellPhone().alarmClock(new Bell(){
+            @Override
+            public void ring(){
+                System.out.println("懒猪起床了");
+            }
+        });
+        new CellPhone().alarmClock(new Bell(){
+            @Override
+            public void ring(){
+                System.out.println("小伙伴上课了");
+            }
+        });
+    }
+}
+interface Bell{
+    void ring();
+}
+class CellPhone{
+    public void alarmClock(Bell bell){
+        bell.ring();
+    }
+}
