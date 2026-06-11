38af4edc1db4fe9c97ef86df61ea965531b8f00a
Zhou Chengxi <267263097+ZHOU11202025@users.noreply.github.com>
2026-06-11 13:48:06 +0800

    Simplify element addition with for loop

--- src/practice/ListOperationDemo.java
+++ src/practice/ListOperationDemo.java
@@ -10,17 +10,9 @@ import java.util.List;
 public class ListOperationDemo {
     public static void main(String[] args) {
         List list = new ArrayList();
-        list.add("1");
-        list.add("2");
-        list.add("3");
-        list.add("4");
-        list.add("5");
-        list.add("6");
-        list.add("7");
-        list.add("8");
-        list.add("9");
-        list.add("10");
-        list.add("11");
+        for(int i = 0; i < 12; i++){
+            list.add("hello" + i);
+        }
         System.out.println(list);
         list.add(1,"韩顺平教育");
         System.out.println("2号位插入元素‘韩顺平教育’:" + list);
