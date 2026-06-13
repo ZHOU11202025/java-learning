0469a34a26e370aada38803300365e1babb732fc
Zhou Chengxi <267263097+ZHOU11202025@users.noreply.github.com>
2026-06-13 21:56:50 +0800

    Complete ArrayListSourceStudy exercise

---
+++ src/practice/ArrayListSourceStudy.java
@@ -0,0 +1,22 @@
+package practice;
+
+import java.util.ArrayList;
+
+public class ArrayListSourceStudy {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        //ArrayList list = new ArrayList(8);
+        for (int i = 1; i <= 10; i++) {
+            list.add(i);
+        }
+        for (int i = 11; i <= 15; i++) {
+            list.add(i);
+        }
+        list.add(100);
+        list.add(200);
+        list.add(null);
+        for (Object object : list) {
+            System.out.println(object);
+        }
+    }
+}
